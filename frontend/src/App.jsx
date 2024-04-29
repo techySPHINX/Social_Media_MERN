101d35b3e32d04a6ff45b6f86c97a8b67b509e0c
techySphinx <shimon123universe@gmail.com>
2024-04-29 19:27:15 +0530

    feat: Authentication- Login and Signup functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,28 @@
 
 import { Container } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
+import Homepage from "./pages/HomePage";
 import Header from "./components/Header";
+import AuthPage from "./pages/AuthPage";
+import userAtom from "./atoms/usersAtom";
+import { useRecoilValue } from "recoil";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
+  const user = useRecoilValue(userAtom);
   return (
     <Container maxW="620px">
       <Header />
       <Routes>
+        <Route path='/' element={user ? <Homepage /> : <Navigate to="/auth"/>} />
+        <Route path='/auth' element={!user ? <AuthPage/> :<Navigate to="/"/>} />
         <Route path="/:username"  element={<UserPage /> }/>
         <Route path="/:username/post/:pid"  element={<PostPage /> }/>
       </Routes>
+
+      {user && <LogoutButton />}
     </Container>
   );
 }
