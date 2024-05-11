938713fa62747bdb3fbe59453fb3e7234311603f
techySphinx <shimon123universe@gmail.com>
2024-05-12 00:08:54 +0530

    feat : UpdateProfile and FollowUnfollow channelised

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -2,107 +2,127 @@ import { Avatar } from "@chakra-ui/avatar";
 import { Box, Flex, Link, Text, VStack } from "@chakra-ui/layout";
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
 import { Portal } from "@chakra-ui/portal";
-import { useToast } from "@chakra-ui/react";
+import { Button, useToast } from "@chakra-ui/react";
 import { BsInstagram } from "react-icons/bs";
 import { CgMoreO } from "react-icons/cg";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/usersAtom";
+import { Link as RouterLink } from "react-router-dom";
+import useFollowUnfollow from "../hooks/useFollowUnfollow";
 
-const UserHeader = () => {
-  const toast = useToast();
+const UserHeader = ({ user }) => {
+	const toast = useToast();
+	const currentUser = useRecoilValue(userAtom); 
+	const { handleFollowUnfollow, following, updating } = useFollowUnfollow(user);
 
-  const copyURL = () => {
-    const currentURL = window.location.href;
-    navigator.clipboard.writeText(currentURL).then(() => {
-      toast({
-        title: "Success.",
-        status: "success",
-        description: "Profile link copied.",
-        duration: 3000,
-        isClosable: true,
-      });
-    });
-  };
 
-  return (
-    <VStack gap={4} alignItems={"start"}>
-      <Flex justifyContent={"space-between"} w={"full"}>
-        <Box>
-          <Text fontSize={"2xl"} fontWeight={"bold"}>
-            Jagan Kumar Hotta
-          </Text>
-          <Flex gap={2} alignItems={"center"}>
-            <Text fontSize={"sm"}>jagankumar</Text>
-            <Text
-              fontSize={"xs"}
-              bg={"gray.dark"}
-              color={"gray.light"}
-              p={1}
-              borderRadius={"full"}
-            >
-              threads.net
-            </Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Avatar name="Jagan Kumar Hotta" src="/zuck-avatar.png" size={
-            {
-              base: "md",
-              md: "xl",
-            }
-          } />
-        </Box>
-      </Flex>
+	const copyURL = () => {
+		const currentURL = window.location.href;
+		navigator.clipboard.writeText(currentURL).then(() => {
+			toast({
+				title: "Success.",
+				status: "success",
+				description: "Profile link copied.",
+				duration: 3000,
+				isClosable: true,
+			});
+		});
+	};
 
-      <Text>Full-Stack Developer, Creating new Projects with New Ideas.</Text>
-      <Flex w={"full"} justifyContent={"space-between"}>
-        <Flex gap={2} alignItems={"center"}>
-          <Text color={"gray.light"}>300 followers</Text>
-          <Box w="1" h="1" bg={"gray.light"} borderRadius={"full"}></Box>
-          <Link color={"gray.light"}>instagram.com</Link>
-        </Flex>
-        <Flex>
-          <Box className="icon-container">
-            <BsInstagram size={24} cursor={"pointer"} />
-          </Box>
-          <Box className="icon-container">
-            <Menu>
-              <MenuButton>
-                <CgMoreO size={24} cursor={"pointer"} />
-              </MenuButton>
-              <Portal>
-                <MenuList bg={"gray.dark"}>
-                  <MenuItem bg={"gray.dark"} onClick={copyURL}>
-                    Copy link
-                  </MenuItem>
-                </MenuList>
-              </Portal>
-            </Menu>
-          </Box>
-        </Flex>
-      </Flex>
+	return (
+		<VStack gap={4} alignItems={"start"}>
+			<Flex justifyContent={"space-between"} w={"full"}>
+				<Box>
+					<Text fontSize={"2xl"} fontWeight={"bold"}>
+						{user.name}
+					</Text>
+					<Flex gap={2} alignItems={"center"}>
+						<Text fontSize={"sm"}>{user.username}</Text>
+						<Text fontSize={"xs"} bg={"gray.dark"} color={"gray.light"} p={1} borderRadius={"full"}>
+							threads.net
+						</Text>
+					</Flex>
+				</Box>
+				<Box>
+					{user.profilePic && (
+						<Avatar
+							name={user.name}
+							src={user.profilePic}
+							size={{
+								base: "md",
+								md: "xl",
+							}}
+						/>
+					)}
+					{!user.profilePic && (
+						<Avatar
+							name={user.name}
+							src='https://bit.ly/broken-link'
+							size={{
+								base: "md",
+								md: "xl",
+							}}
+						/>
+					)}
+				</Box>
+			</Flex>
 
-      <Flex w={"full"}>
-        <Flex
-          flex={1}
-          borderBottom={"1.5px solid white"}
-          justifyContent={"center"}
-          pb="3"
-          cursor={"pointer"}
-        >
-          <Text fontWeight={"bold"}> Threads</Text>
-        </Flex>
-        <Flex
-          flex={1}
-          borderBottom={"1px solid gray"}
-          justifyContent={"center"}
-          color={"gray.light"}
-          pb="3"
-          cursor={"pointer"}
-        >
-          <Text fontWeight={"bold"}> Replies</Text>
-        </Flex>
-      </Flex>
-    </VStack>
-  );
+			<Text>{user.bio}</Text>
+
+			{currentUser?._id === user._id && (
+				<Link as={RouterLink} to='/update'>
+					<Button size={"sm"}>Update Profile</Button>
+				</Link>
+			)}
+			{currentUser?._id !== user._id && (
+				<Button size={"sm"} onClick={handleFollowUnfollow} isLoading={updating}>
+					{following ? "Unfollow" : "Follow"}
+				</Button>
+			)}
+			<Flex w={"full"} justifyContent={"space-between"}>
+				<Flex gap={2} alignItems={"center"}>
+					<Text color={"gray.light"}>{user.followers.length} followers</Text>
+					<Box w='1' h='1' bg={"gray.light"} borderRadius={"full"}></Box>
+					<Link color={"gray.light"}>instagram.com</Link>
+				</Flex>
+				<Flex>
+					<Box className='icon-container'>
+						<BsInstagram size={24} cursor={"pointer"} />
+					</Box>
+					<Box className='icon-container'>
+						<Menu>
+							<MenuButton>
+								<CgMoreO size={24} cursor={"pointer"} />
+							</MenuButton>
+							<Portal>
+								<MenuList bg={"gray.dark"}>
+									<MenuItem bg={"gray.dark"} onClick={copyURL}>
+										Copy link
+									</MenuItem>
+								</MenuList>
+							</Portal>
+						</Menu>
+					</Box>
+				</Flex>
+			</Flex>
+
+			<Flex w={"full"}>
+				<Flex flex={1} borderBottom={"1.5px solid white"} justifyContent={"center"} pb='3' cursor={"pointer"}>
+					<Text fontWeight={"bold"}> Threads</Text>
+				</Flex>
+				<Flex
+					flex={1}
+					borderBottom={"1px solid gray"}
+					justifyContent={"center"}
+					color={"gray.light"}
+					pb='3'
+					cursor={"pointer"}
+				>
+					<Text fontWeight={"bold"}> Replies</Text>
+				</Flex>
+			</Flex>
+		</VStack>
+	);
 };
 
 export default UserHeader;
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,15 +1,61 @@
 import UserHeader from "../components/UserHeader";
 import UserPost from "../components/UserPost";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
 
 const UserPage = () => {
-  return(
+  const [user, setUser] = useState(null);
+  const { username } = useParams();
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setUser(data);
+      } catch (error) {
+        showToast("Error", error, "error");
+      }
+    };
+
+    getUser();
+  }, [username, showToast]);
+
+  if(!user) return null;
+
+  return (
     <>
-    <UserHeader />   {/*gives information about the header */}
-    <UserPost likes={1200} replies={481} postImg="/post1.png" postTitle="Let's talk about Development." />
-    <UserPost likes={1201} replies={483} postImg="/post2.png" postTitle="Let's talk about Development." />
-    <UserPost likes={129} replies={487} postImg="/post3.png" postTitle="Let's talk about Development." />
-    <UserPost likes={126} replies={486}  postTitle="Let's talk about Development." />
-     </>
+      <UserHeader user={user} />
+      <UserPost
+        likes={1200}
+        replies={481}
+        postImg="/post1.png"
+        postTitle="Let's talk about Development."
+      />
+      <UserPost
+        likes={1201}
+        replies={483}
+        postImg="/post2.png"
+        postTitle="Let's talk about Development."
+      />
+      <UserPost
+        likes={129}
+        replies={487}
+        postImg="/post3.png"
+        postTitle="Let's talk about Development."
+      />
+      <UserPost
+        likes={126}
+        replies={486}
+        postTitle="Let's talk about Development."
+      />
+    </>
   );
 };
 
